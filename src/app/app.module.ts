2a4b309640ea49a04dda35d911b6063379a51a52
David <davidb1101@gmail.com>
2023-09-17 15:38:56 -0500

    added in calendar views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DayViewComponent } from './calendar-views/day/day-view/day-view.component';
+import { WeekViewComponent } from './calendar-views/week/week-view/week-view.component';
+import { MonthViewComponent } from './calendar-views/month/month-view/month-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DayViewComponent,
+    WeekViewComponent,
+    MonthViewComponent
   ],
   imports: [
     BrowserModule,
