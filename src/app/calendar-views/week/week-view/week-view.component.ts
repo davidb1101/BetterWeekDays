86236a38442bfbc156667f4641dc398d9d3cae23
David <davidb1101@gmail.com>
2023-09-17 18:32:03 -0500

    Displaying week with updated week day names

--- src/app/calendar-views/week/week-view/week-view.component.ts
+++ src/app/calendar-views/week/week-view/week-view.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomDayName } from '../../../models/custom-day-names'
+import { MonthName } from '../../../models/month-names'
+
+
+export interface WeekDay {
+  dayOfWeek: number,
+  dayName: string,
+}
 
 @Component({
   selector: 'app-week-view',
@@ -6,10 +14,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./week-view.component.scss']
 })
 export class WeekViewComponent implements OnInit {
+  public currentDate: Date = new Date();
+  public dayOfWeek: number = 0;
+  public dayName: string = "";
+  public monthName: string = "";
+  public dayNumber: number = 0;
 
-  constructor() { }
+  public weekDayList: WeekDay[] = Array<WeekDay>();
+
+  constructor() {
+    this.dayNumber = this.currentDate.getDay();
+    for(let i = 1; i < 7; i++)
+    {
+      this.weekDayList.push({dayOfWeek: i, dayName: CustomDayName[i]})
+    }
+    this.weekDayList.push({dayOfWeek: 7, dayName: CustomDayName[0]})
+    console.log(this.weekDayList);
+  }
 
   ngOnInit(): void {
   }
 
 }
+
+
