a687c98e8d78d8060a86a5d541d30f18e85dc9ef
David <davidb1101@gmail.com>
2023-09-17 15:44:46 -0500

    added in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { DayViewComponent } from './calendar-views/day/day-view/day-view.component';
+import { WeekViewComponent } from './calendar-views/week/week-view/week-view.component';
+import { MonthViewComponent } from './calendar-views/month/month-view/month-view.component';
+
 const routes: Routes = [
+  {
+    path: "day-view",
+    component: DayViewComponent,
+  },
+  {
+    path: "week-view",
+    component: WeekViewComponent,
+  },
+  {
+    path: "month-view",
+    component: MonthViewComponent,
+  }
 ];
 
 @NgModule({
